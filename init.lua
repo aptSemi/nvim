9a25935c9ecbb6b48afa734d8c4de6d96439f307
aptsemi <aptsemi@protonmail.com>
2022-10-08 17:59:07 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- Leader key is Spacebar
+vim.g.mapleader = " "
+
+-- Options
+vim.o.termguicolors = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.incsearch = true
+vim.o.hidden = true
+vim.o.smartindent = true
+vim.o.number = true
+vim.o.wrap = true
+vim.o.mouse = "niv"
+vim.o.ignorecase = true
+vim.o.cursorline = true
+--vim.o.splitright = true
+
+-- Plugins
+require('packer-plugins')
---
+++ lua/packer-plugins.lua
@@ -0,0 +1,15 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'preservim/nerdtree'
+
+  --colorscheme
+  use { "ellisonleao/gruvbox.nvim" }
+
+
+
+
+
+
+end)
