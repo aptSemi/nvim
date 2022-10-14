9623df52069aa27bf458d7e1acb3de8d6cbaf162
aptsemi <aptsemi@protonmail.com>
2022-10-13 20:50:11 -0700

    added to options file

--- lua/options.lua
+++ lua/options.lua
@@ -5,18 +5,19 @@
   vim.opt.termguicolors = true                -- set term gui colors (most terminals support this)
   vim.opt.number = true                       -- set numbered lines
   vim.opt.relativenumber = true               -- set relative numbered lines
-  vim.opt.tabstop = 2                         -- insert 2 spaces for a tab
-  vim.opt.softtabstop = 2                     -- ???
+  vim.opt.tabstop = 4                         -- insert 4 (or 2) spaces for a tab
+  vim.opt.softtabstop = 4                     -- number of spaces TAB uses while editing
   vim.opt.expandtab = true                    -- converts tab to spaces
-  vim.opt.incsearch = true                    -- incremental search
-  vim.opt.hidden = true                       -- ???
+  vim.opt.incsearch = true                    -- highlight match while typing search pattern
+  vim.opt.hidden = true                       -- do not unload buffer when abandoned
   vim.opt.smartindent = true                  -- make indenting smarter again
   vim.opt.wrap = true                         -- line wraps instead of going off screen
   vim.opt.mouse = "a"                         -- mouse support "a" = all modes
+  vim.opt.smartcase = true                    -- no ignore case when pattern has upper-case
   vim.opt.ignorecase = true                   -- ignore case in search patterns
   vim.opt.cursorline = true                   -- highlight the current line
   vim.opt.splitright = true                   -- force vertical splits to the right
   vim.opt.backup = false                      -- creates backup file (NO!)
   vim.opt.fileencoding = "utf-8"              -- the encoding written to file
   vim.opt.swapfile = false                    -- create swap file (NO!)
-
+  vim.opt.updatetime = 50                     -- better update time (default 4000 ms = 4 sec)
