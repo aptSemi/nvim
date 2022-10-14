811d0566875cad535772c7857265916ea21d8ea9
aptsemi <aptsemi@protonmail.com>
2022-10-14 08:11:36 -0700

    cleaned up configuration files

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
--- Leader key is Spacebar
-vim.g.mapleader = " "
-
-
 -- Plugins
-require('packer-plugins')  -- lua/packer-plugins.lua
-require('lualine')         -- plugin/lualine.lua
+require('packer-plugins')      -- lua/packer-plugins.lua
+require('lualine')             -- plugin/lualine.lua
 require('Comment').setup() 
-require('options')        -- lua/options.lua
+require('options')             -- lua/options.lua
 
--- lua/options.lua
+++ lua/options.lua
@@ -1,23 +1,34 @@
+-- Essentials
+vim.g.mapleader = " "
 
+-- Behavior
+vim.opt.hidden = true                       -- do not unload buffer when abandoned
+vim.opt.splitright = true                   -- force vertical splits to the right
+vim.opt.backup = false                      -- creates backup file (NO!)
+vim.opt.swapfile = false                    -- create swap file (NO!)
+vim.opt.updatetime = 50                     -- better update time (default 4000 ms = 4 sec)
+vim.opt.wildmenu = true                     -- tab complete on commandline
+--vim.opt.undofile = true                     -- undo file?  Yes!  (for future builds; currently not in use)
 
-  -- General Options
-  --
-  vim.opt.termguicolors = true                -- set term gui colors (most terminals support this)
-  vim.opt.number = true                       -- set numbered lines
-  vim.opt.relativenumber = true               -- set relative numbered lines
-  vim.opt.tabstop = 4                         -- insert 4 (or 2) spaces for a tab
-  vim.opt.softtabstop = 4                     -- number of spaces TAB uses while editing
-  vim.opt.expandtab = true                    -- converts tab to spaces
-  vim.opt.incsearch = true                    -- highlight match while typing search pattern
-  vim.opt.hidden = true                       -- do not unload buffer when abandoned
-  vim.opt.smartindent = true                  -- make indenting smarter again
-  vim.opt.wrap = true                         -- line wraps instead of going off screen
-  vim.opt.mouse = "a"                         -- mouse support "a" = all modes
-  vim.opt.smartcase = true                    -- no ignore case when pattern has upper-case
-  vim.opt.ignorecase = true                   -- ignore case in search patterns
-  vim.opt.cursorline = true                   -- highlight the current line
-  vim.opt.splitright = true                   -- force vertical splits to the right
-  vim.opt.backup = false                      -- creates backup file (NO!)
-  vim.opt.fileencoding = "utf-8"              -- the encoding written to file
-  vim.opt.swapfile = false                    -- create swap file (NO!)
-  vim.opt.updatetime = 50                     -- better update time (default 4000 ms = 4 sec)
+-- Look and Feel
+vim.opt.number = true                       -- set numbered lines
+vim.opt.relativenumber = true               -- set relative numbered lines
+vim.opt.mouse = "a"                         -- mouse support "a" = all modes
+vim.opt.wrap = true                         -- line wraps instead of going off screen
+vim.opt.cursorline = true                   -- highlight the current line
+vim.opt.errorbells = false                  -- no annoying bells!
+vim.opt.fileencoding = "utf-8"              -- the encoding written to file
+
+-- Searching
+vim.opt.smartcase = true                    -- no ignore case when pattern has upper-case
+vim.opt.ignorecase = true                   -- ignore case in search patterns
+vim.opt.incsearch = true                    -- highlight match while typing search pattern
+
+-- Indentation
+vim.opt.tabstop = 4                         -- insert 4 (or 2) spaces for a tab
+vim.opt.softtabstop = 4                     -- number of spaces TAB uses while editing
+vim.opt.expandtab = true                    -- converts tab to spaces
+vim.opt.smartindent = true                  -- make indenting smarter again
+
+-- Colors
+vim.opt.termguicolors = true                -- set term gui colors (most terminals support this)
