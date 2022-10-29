cbc47fa37e2017dcd7b6b798de9c1a27000b621c
aptsemi <aptsemi@protonmail.com>
2022-10-28 22:44:04 -0700

    added new options

--- lua/semi/options.lua
+++ lua/semi/options.lua
@@ -4,6 +4,7 @@ vim.g.mapleader = " "
 -- Behavior
 vim.opt.hidden = true                       -- do not unload buffer when abandoned
 vim.opt.splitright = true                   -- force vertical splits to the right
+vim.opt.splitbelow = true                   -- force horizontal splits to go below current window
 vim.opt.backup = false                      -- creates backup file (NO!)
 vim.opt.swapfile = false                    -- create swap file (NO!)
 vim.opt.updatetime = 50                     -- better update time (default 4000 ms = 4 sec)
@@ -11,6 +12,7 @@ vim.opt.wildmenu = true                     -- tab complete on commandline
 vim.opt.inccommand = "nosplit"              -- preview %s commands live as they're being typed
 vim.opt.belloff = "all"                     -- NO BELLS!!!
 vim.opt.errorbells = false                  -- no annoying bells!
+vim.opt.clipboard - "unnamedplus"           -- allow nvim to access system clipboard
 --vim.opt.undofile = true                     -- undo file?  Yes!  (for future builds; currently not in use)
 
 -- Look and Feel
@@ -18,6 +20,7 @@ vim.opt.number = true                       -- set numbered lines
 vim.opt.relativenumber = true               -- set relative numbered lines
 vim.opt.mouse = "a"                         -- mouse support "a" = all modes
 vim.opt.wrap = true                         -- line wraps instead of going off screen
+vim.opt.linebreak = true                    -- companion to 'wrap", do not split words
 vim.opt.cursorline = true                   -- highlight the current line
 vim.opt.fileencoding = "utf-8"              -- the encoding written to file
 
