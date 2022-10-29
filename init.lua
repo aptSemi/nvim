0b06bc091b8757dd6c7672a736e9126d39994eff
aptsemi <aptsemi@protonmail.com>
2022-10-28 22:34:40 -0700

    structure changes

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Plugins
-require('semi.plugins')      -- lua/semi/plugins.lua
-require('lualine')             -- plugin/lualine.lua
-require('Comment').setup() 
+require('semi.plugins')             -- lua/semi/plugins.lua
 require('semi.options')             -- lua/semi/options.lua
+require('lualine')                  -- plugin/lualine.lua
+require('Comment').setup() 
 require("bufferline").setup{}
 require("nvim-tree").setup()
