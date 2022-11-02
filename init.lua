492434b5525c4e8072d8d3446574fe3a4169e1c8
aptsemi <aptsemi@protonmail.com>
2022-11-01 19:16:58 -0700

    added plugins, cleaned up files

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
+
 -- Plugins
---
-require('user.plugins')             -- /lua/semi/plugins.lua
-require('user.options')             -- /lua/semi/options.lua
---require('user.keymaps')             -- /lua/semi.keymaps.lua
+
+require('user.plugins')             -- /lua/user/plugins.lua
+require('user.options')             -- /lua/user/options.lua
 require('user.lualine')             -- /plugin/lualine.lua
 require('Comment').setup() 
-require("bufferline").setup{}
+require("bufferline").setup()
 require("nvim-tree").setup()
---require('user.treesitter)
-require("user.colorscheme")         -- /lua/semi/colorscheme.lua (change colorschemes here)
+--require('user.treesitter')           -- /lua/user/treesitter.lua
+require('user.keymaps')             -- /lua/user/keymaps.lua
+require('user.lualine')             -- /plugin/lualine.lua
+require("user.colorscheme")         -- /lua/user/colorscheme.lua (change colorschemes here)
