1db9aac2dd19ad4ef1dc8d91aa3e61b7d8ec34d3
aptsemi <aptsemi@protonmail.com>
2022-11-03 15:31:16 -0700

    added telescope plugin

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@
 
 require('user.plugins')             -- /lua/user/plugins.lua
 require('user.options')             -- /lua/user/options.lua
-require('user.lualine')             -- /plugin/lualine.lua
 require('Comment').setup() 
 require("bufferline").setup()
 require("nvim-tree").setup()
 require('user.treesitter')           -- /lua/user/treesitter.lua
 require('user.keymaps')             -- /lua/user/keymaps.lua
 require('user.lualine')             -- /plugin/lualine.lua
+require('user.telescope')           -- /lua/user/telescope.lua
 require("user.colorscheme")         -- /lua/user/colorscheme.lua (change colorschemes here)
