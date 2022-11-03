44a69cc185f8e5c28d7d511d826433f2dc4a65ac
aptsemi <aptsemi@protonmail.com>
2022-11-03 15:41:42 -0700

    better comments for anyone that wants to use my trash config

--- init.lua
+++ init.lua
@@ -1,13 +1,12 @@
-
 -- Plugins
 
 require('user.plugins')             -- /lua/user/plugins.lua
 require('user.options')             -- /lua/user/options.lua
-require('Comment').setup() 
-require("bufferline").setup()
-require("nvim-tree").setup()
-require('user.treesitter')           -- /lua/user/treesitter.lua
+require('Comment').setup()          -- default setup
+require("bufferline").setup()       -- default setup
+require("nvim-tree").setup()        -- default setup (change toggle keymap in /lua/user/keymaps.lua)
+require('user.treesitter')          -- /lua/user/treesitter.lua
 require('user.keymaps')             -- /lua/user/keymaps.lua
-require('user.lualine')             -- /plugin/lualine.lua
+require('user.lualine')             -- /lua/user/lualine.lua
 require('user.telescope')           -- /lua/user/telescope.lua
 require("user.colorscheme")         -- /lua/user/colorscheme.lua (change colorschemes here)
