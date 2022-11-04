9d8f3cb3d74fb727495907dcc77cc629434e8404
aptsemi <aptsemi@protonmail.com>
2022-11-04 15:08:18 -0700

    file cleanup

--- init.lua
+++ init.lua
@@ -2,13 +2,13 @@
 
 require('user.plugins')             -- /lua/user/plugins.lua
 require('user.options')             -- /lua/user/options.lua
-require('Comment').setup()          -- default setup (default is good)
-require("bufferline").setup()       -- default setup (for now)
-require('gitsigns').setup()         -- default setup (for now)
-require("toggleterm").setup()       -- default setup (for now)
-require("nvim-tree").setup()        -- default setup (change toggle keymap in /lua/user/keymaps.lua)
 require('user.treesitter')          -- /lua/user/treesitter.lua
 require('user.keymaps')             -- /lua/user/keymaps.lua
 require('user.lualine')             -- /lua/user/lualine.lua
 require('user.telescope')           -- /lua/user/telescope.lua
-require("user.colorscheme")         -- /lua/user/colorscheme.lua (change colorschemes here)
+require('user.colorscheme')         -- /lua/user/colorscheme.lua (change colorschemes here)
+require('user.toggleterm')--.setup()       -- remove user. and uncomment setup section to get default settings
+require('Comment').setup()          -- default setup (default is good)
+require('gitsigns').setup()         -- default setup (for now)
+require('bufferline').setup()       -- default setup (for now)
+require('nvim-tree').setup()        -- default setup (change toggle keymap in /lua/user/keymaps.lua)
