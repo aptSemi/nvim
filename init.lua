4eecbf0eb4a8df5e8232bb2561425dad7723cb9f
aptsemi <aptsemi@protonmail.com>
2022-11-04 13:49:20 -0700

    added terminal plugin

--- init.lua
+++ init.lua
@@ -2,9 +2,10 @@
 
 require('user.plugins')             -- /lua/user/plugins.lua
 require('user.options')             -- /lua/user/options.lua
-require('Comment').setup()          -- default setup
-require("bufferline").setup()       -- default setup
-require('gitsigns').setup()         -- default setup
+require('Comment').setup()          -- default setup (default is good)
+require("bufferline").setup()       -- default setup (for now)
+require('gitsigns').setup()         -- default setup (for now)
+require("toggleterm").setup()       -- default setup (for now)
 require("nvim-tree").setup()        -- default setup (change toggle keymap in /lua/user/keymaps.lua)
 require('user.treesitter')          -- /lua/user/treesitter.lua
 require('user.keymaps')             -- /lua/user/keymaps.lua
