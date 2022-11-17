b9828f31b8db6639bcaa4f28f38f2c09dd16dc96
aptsemi <aptsemi@protonmail.com>
2022-11-16 16:01:05 -0800

    test line

--- init.lua
+++ init.lua
@@ -12,3 +12,4 @@ require('user.toggleterm')          -- /lua/user/toggleterm.lua
 require('user.treesitter')          -- /lua/user/treesitter.lua
 require('user.cmp')                 -- /lua/user/cmp.lua
 -- require('gitsigns').setup()         -- CAUSING ERRORS?? default config
+-- require('gitsigns').setup()         -- CAUSING ERRORS?? default config
