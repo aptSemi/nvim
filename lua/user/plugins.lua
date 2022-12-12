8c49d51d611a09593f9afdd2206a04e7428320a0
aptsemi <joemckellar399@gmail.com>
2022-12-12 10:22:18 -0800

    fixed luasnip

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,13 +1,13 @@
 local fn = vim.fn
 
--- Automatically install packer
+-- automatically install packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   print("Installing packer close and reopen Neovim...")
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -15,13 +15,13 @@ vim.cmd([[
   augroup end
 ]])
 
--- Use a protected call so we don't error out on first use
+-- use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
 
--- Have packer use a popup window
+-- have packer use a popup window
 packer.init({
     display = {
       open_fn = function()
@@ -31,7 +31,7 @@ packer.init({
   }
 )
 
--- Install your plugins here
+-- install your plugins here
 return packer.startup(function(use)
 
 -- plugins --
@@ -57,7 +57,7 @@ use 'hrsh7th/cmp-buffer'                        -- buffer completions
 use 'hrsh7th/cmp-path'                          -- path completions
 
 -- snippets --
-use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
+use 'L3MON4D3/LuaSnip'
 
 -- lsp --
 use {
@@ -81,8 +81,8 @@ use 'Matsuuu/pinkmare'
 use 'frenzyexists/aquarium-vim'
 use 'navarasu/onedark.nvim'
 
--- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
+-- automatically set up your configuration after cloning packer.nvim
+  -- put this at the end after all plugins
   if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
