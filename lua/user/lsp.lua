c9b8c9e7318f166af9609408fdd8399fa21d9400
aptsemi <joemckellar399@gmail.com>
2022-12-19 17:35:22 -0800

    starting remaps

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,4 +1,11 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+-- lsp.on_attach(function(client, bufnr)
+--   local opts = {buffer = bufnr, remap - false}
+--
+-- )
+
+
 lsp.setup()
