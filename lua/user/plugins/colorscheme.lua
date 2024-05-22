61a12323297b735b1abd7fe84afb79fe3becedad
aptsemi <joemckellar399@gmail.com>
2024-05-22 16:16:26 -0700

    added colorschemes

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -1,7 +1,23 @@
 return {
-  'sainnhe/sonokai',
+  "sainnhe/sonokai",
   priority = 1000,
   config = function()
-    vim.cmd('colorscheme sonokai')
-  end
+    vim.cmd("colorscheme sonokai")
+  end,
 }
+
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme gruvbox")
+--   end,
+-- }
+
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme kanagawa")
+--   end,
+-- }
