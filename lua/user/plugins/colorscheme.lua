e913200f1bb09b4166e873858cec95c19faaedea
aptsemi <joemckellar399@gmail.com>
2024-10-29 14:17:57 -0700

    added edge colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -21,10 +21,18 @@
 --   end,
 -- }
 
+-- return {
+--   "morhetz/gruvbox",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme gruvbox")
+--   end,
+-- }
+
 return {
-  "morhetz/gruvbox",
+  "sainnhe/edge",
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme gruvbox")
+    vim.cmd("colorscheme edge")
   end,
 }
