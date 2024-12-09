499c052832de9faca131449d32bb4462ac04af9f
aptsemi <joemckellar399@gmail.com>
2024-12-08 17:44:23 -0800

    premptive add of new install

--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -1,8 +1,21 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  tag = "v3.8.2", -- remove tag & enabled when a working updated version is released!
+  enabled = true,
   event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
   opts = {
     indent = { char = "┊" },
   },
 }
+
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   event = { "BufReadPre", "BufNewFile" },
+--   main = "ibl",
+--   ---@module "ibl"
+--   ---@type ibl.config
+--   opts = {
+--     indent = { char = "┊" },
+--   },
+-- }
